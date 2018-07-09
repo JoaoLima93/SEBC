24c99f0d1a253131b875f223b6dbb43c025ee90c
João Lima <joao.lima@amaro.com>
2018-07-09 14:32:18 -0300

    :sparkles: Include Senser

--- SEBC_php/salvar.php
+++ SEBC_php/salvar.php
@@ -1,14 +1,20 @@
 <?php
+	include ('conexao.php');
 
 	$id_cliente 	= $_GET['id_cliente'];
-	$leitura	  	= $_GET['leitura'];
-	$hora_consumo 	= $_GET['hora_consumo'];
+	$leitura_a	  	= $_GET['leitura'];
+	$hora_a		 	= $_GET['hora_consumo'];
 
-	$sql = "INSERT INTO leituras (id_cliente, leitura, hora_consumo) VALUES (:id_cliente, :leitura, :hora_consumo)";
+	$sql = "INSERT INTO leituras (id_cliente, leitura_a, hora_a) VALUES (:id_cliente, :leitura_a, :hora_a)";
 
 	$stmt = $PDO -> prepare($sql);
 	$stmt -> bindParam(':id_cliente', $id_cliente);
-	$stmt -> bindParam(':leitura', $leitura);
-	$stmt -> bindParam(':hora_consumo', $hora_consumo);
+	$stmt -> bindParam(':leitura_a', $leitura_a);
+	$stmt -> bindParam(':hora_a', $hora_a);
 
+	if ($stmt -> execute()) {
+		echo 'Ok';
+	} else {
+		echo "Error";
+	}
 ?> 
