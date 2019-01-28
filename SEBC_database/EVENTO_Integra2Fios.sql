5ea2c5f73f28f12f48008845e86e6a9c6a2702f4
João Lima <joao.lima@amaro.com>
2019-01-28 13:28:52 -0200

    :card_file_box: Correct event calculation

--- SEBC_database/EVENTO_Integra2Fios.sql
+++ SEBC_database/EVENTO_Integra2Fios.sql
@@ -1,5 +1,5 @@
 
--- Integração de instalações elétricas com 2 fios.
+-- Integração de instalações elétricas com 2 fios por minuto
 -- Cliente Monofásico (1Fases e 2Fios) e Bifásico (2 Fases e 2 Fios)
    CREATE EVENT integra_consumo_2fios
    ON SCHEDULE EVERY 1 MINUTE
@@ -7,11 +7,11 @@
    DO insert into CALC_CONSUMO (ID_CLIENTE, CONSUMO, CUSTO_TARIFA_NORMAL, CUSTO_TARIFA_BRANCA, DATA_CONSUMO) 	
       select 	        
              cc.id as ID_CLIENTE,	        
- 		     (sum(l.leitura_a)/count(l.leitura_a))                                       as CONSUMO, 	       
-             (sum(l.leitura_a)/count(l.leitura_a))*avg(tc.preco_kwh)                     as CUSTO_TARIFA_NORMAL,
-             if(f.dia_util='T',(sum(l.leitura_a)/count(l.leitura_a))*avg(tb.preco_kwh)	        
-      						  ,(sum(l.leitura_a)/count(l.leitura_a))*avg(tbf.preco_kwh)) as CUSTO_TARIFA_BRANCA,
- 	         max(l.hora_web)                                                             as DATA_CONSUMO	             
+ 		     (sum(l.leitura_a)/count(l.leitura_a))                                            as CONSUMO, 	       
+             ((sum(l.leitura_a)/count(l.leitura_a))/60)*avg(tc.preco_kwh)                     as CUSTO_TARIFA_NORMAL,
+             if(f.dia_util='T',((sum(l.leitura_a)/count(l.leitura_a))/60)*avg(tb.preco_kwh)	        
+      						  ,((sum(l.leitura_a)/count(l.leitura_a))/60)*avg(tbf.preco_kwh)) as CUSTO_TARIFA_BRANCA,
+ 	         max(l.hora_web)                                                                  as DATA_CONSUMO	             
       from leituras l 	        
  		left join cadastro_cliente cc on cc.id =l.id_cliente				
       left join tarifas tb  on  tb.inicio_periodo < time(l.hora_consumo)           
--- SEBC_database/EVENTO_Integra3Fios.sql
+++ SEBC_database/EVENTO_Integra3Fios.sql
@@ -1,17 +1,17 @@
 
--- Integração de instalações elétricas com 2 fios.
+-- Integração de instalações elétricas com 3 fios por minuto
 -- Cliente Bifásico (2 Fases e 3 Fios) e Trifásico (3 Fases e 3 Fios)
-   CREATE EVENT integra_consumo_2fios
+   CREATE EVENT integra_consumo_3fios
    ON SCHEDULE EVERY 1 MINUTE
    COMMENT 'Integraliza o consumo do cliente com 3 fios por minuto'
    DO insert into CALC_CONSUMO (ID_CLIENTE, CONSUMO, CUSTO_TARIFA_NORMAL, CUSTO_TARIFA_BRANCA, DATA_CONSUMO) 	
       select 	        
              cc.id as ID_CLIENTE,	        
- 		     (sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))                           			   as CONSUMO, 	       
-             (sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))*avg(tc.preco_kwh)       			   as CUSTO_TARIFA_NORMAL,
-             if(f.dia_util='T',(sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))*avg(tb.preco_kwh)	        
-      						  ,(sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))*avg(tbf.preco_kwh)) as CUSTO_TARIFA_BRANCA,
- 	         max(l.hora_web)                                                             			   as DATA_CONSUMO	             
+ 		     (sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))/60                         			    as CONSUMO, 	       
+             ((sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))/60)*avg(tc.preco_kwh)       			    as CUSTO_TARIFA_NORMAL,
+             if(f.dia_util='T',((sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))/60)*avg(tb.preco_kwh)	        
+      						  ,((sum(l.leitura_a + l.leitura_b)/count(l.leitura_a))/60)*avg(tbf.preco_kwh)) as CUSTO_TARIFA_BRANCA,
+ 	         max(l.hora_web)                                                             			        as DATA_CONSUMO	             
       from leituras l 	        
  		left join cadastro_cliente cc on cc.id =l.id_cliente				
       left join tarifas tb  on  tb.inicio_periodo < time(l.hora_consumo)           
